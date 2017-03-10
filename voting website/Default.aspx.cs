fcf3537015b570154cfdb36f19fc2b857a00a98e
ruojin <caoruojin@gmail.com>
2017-03-09 19:10:42 -0500

    Add files via upload

---
+++ voting website/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void vote_click(object sender, EventArgs e)
+    {
+        if(RadioButtonList1.SelectedItem == null)
+        {
+            Label1.Text = "Please choose a candidate.";
+        }
+        else
+        {
+            SqlDataSource1.Update();
+            Response.Redirect("Votes.aspx");
+             
+        }
+    }
+}
